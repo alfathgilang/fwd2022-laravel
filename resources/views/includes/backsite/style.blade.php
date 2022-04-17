886b6f3c43894af86aa1e857fb5dc1e1bb2c0f1e
alfathgilang <alfathgilang44@gmail.com>
2022-04-17 22:12:43 +0700

    update cms admin

---
+++ resources/views/includes/backsite/style.blade.php
@@ -0,0 +1,23 @@
+<!-- BEGIN: Vendor CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/vendors/css/vendors.min.css') }}">
+<!-- END: Vendor CSS-->
+
+<!-- BEGIN: Theme CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/css/bootstrap.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/css/bootstrap-extended.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/css/colors.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/css/components.css') }}">
+<!-- END: Theme CSS-->
+
+<!-- BEGIN: Page CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/css/core/menu/menu-types/vertical-menu.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/css/core/colors/palette-gradient.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/app-assets/css/core/colors/palette-tooltip.css') }}">
+<!-- END: Page CSS-->
+
+<!-- BEGIN: Custom CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset('/assets/backsite/assets/css/style.css') }}">
+<!-- END: Custom CSS-->
+
+{{-- Third party --}}
+<link href="{{ url('https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css') }}" rel="stylesheet">
--- resources/views/pages/backsite/dashboard/index.blade.php
+++ resources/views/pages/backsite/dashboard/index.blade.php
@@ -1,1 +1,13 @@
-HOME
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="content-wrapper">
+
+        </div>
+    </div>
+@endsection
